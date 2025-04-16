bc2302b7c27d7a803eef1e78c04bd54995c4ad47
Félix Allard <xilef992@gmail.com>
2025-04-16 11:37:36 -0400

    Just commenting a tiny winny bit my code

--- ASADiscordBot/SRC/SlashCommand/SlashCommandManager.cs
+++ ASADiscordBot/SRC/SlashCommand/SlashCommandManager.cs
@@ -9,14 +9,28 @@ using System;
 using System.Linq;
 using System.Reflection;
 
+/// <summary>
+/// Responsible for handling slash commands
+/// </summary>
 public class SlashCommandManager
 {
+    /// <summary>
+    /// Singleton Class
+    /// </summary>
     public static SlashCommandManager Instance { get; } = new SlashCommandManager();
 
     private DiscordSocketClient client;
     private List<ISlashCommand> slashCommands= new List<ISlashCommand>();
     private IServiceProvider _serviceProvider;
-    public static SlashCommandManager Init(DiscordSocketClient client, 
+    /// <summary>
+    /// This will act as a constructor
+    /// </summary>
+    /// <param name="client">The discord socket client created in the Main Class</param>
+    /// <param name="services">The service provider for dependency injection</param>
+    /// <param name="httpClientFactory">HttpClientFactory so we can create http workers fast and efficiently</param>
+    /// <returns></returns>
+    public static SlashCommandManager Init(
+        DiscordSocketClient client, 
         IServiceProvider services,
         IHttpClientFactory httpClientFactory
     )
@@ -25,7 +39,10 @@ public class SlashCommandManager
         Instance._serviceProvider = services;
         return Instance;
     }
-    
+    /// <summary>
+    /// Creates all the slash commands and register them on discord!
+    /// </summary>
+    /// <returns>Func Task which is the necessary return type since we want some async process while allowing the system multithreading</returns>
     public Func<Task> Build()
     {
         return async () =>
@@ -70,6 +87,10 @@ public class SlashCommandManager
             await Task.WhenAll(addingTasks); // Await all adding tasks too!
         };
     }
+    /// <summary>
+    /// Will get called whenever a slash command is executed and will redirect to the right endpoint
+    /// </summary>
+    /// <param name="command">The command provided by the Discord API</param>
     public async Task SlashCommandHandler(SocketSlashCommand command){
         var foundFunction = slashCommands.FirstOrDefault(x => x.Name == command.Data.Name);
         SocketUser socketUser = command.User as SocketUser;
